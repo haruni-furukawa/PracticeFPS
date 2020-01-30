29b57ce481efef7d33858478469c47c9d06992d0
nusashi <nusashi@gmail.com>
2020-01-31 00:44:02 +0900

    隠しアイテム取得プレハブを実装

---
+++ FPS/Assets/FPS/Scripts/HiddenBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenBox : MonoBehaviour
+{
+	public GameObject giftItem;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (giftItem != null)
+		{
+			GameObject item = Instantiate (giftItem, transform.position, Quaternion.identity);
+			item.transform.parent = null;
+		}
+		Destroy (gameObject);
+	}
+}
